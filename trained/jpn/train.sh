35bbd273d54cc1d606d7cdef063ca43af8b6aad5
Pone Z Pyo <phonezawphyo@gmail.com>
2017-01-16 11:34:19 +0900

    [add]improved jpn

--- trained/jpn/train.sh
+++ trained/jpn/train.sh
@@ -1,5 +1,5 @@
 #fonts=( TakaoExGothic TakaoGothic TakaoPGothic Lato 'Lato Bold' )
-fonts=( lato latob lato.kana latob.kana hiraginokakugothicpro hiraginomarugothicpro osaka )
+fonts=( lato latob lato.kana latob.kana hiraginokakugothicpro hiraginokakugothicstdb hiraginomarugothicpro osaka )
 bases=()
 boxes=()
 trs=()
@@ -45,6 +45,8 @@ mftraining -F jpn.font_properties -U unicharset -O unicharset $joined_trs
 
 cntraining -F jpn.font_properties -U unicharset $joined_trs
 
+wordlist2dawg jpn.words_list jpn.word-dawg jpn.unicharset
+
 mv shapetable jpn.shapetable
 mv normproto jpn.normproto
 mv inttemp jpn.inttemp
@@ -58,12 +60,16 @@ mv jpn.traineddata ./tessdata
 
 echo pgoname
 tesseract pgoname.tiff test.pgoname --tessdata-dir ./ -l jpn && cat test.pgoname.txt
+echo pokemonnamethres
+tesseract pokemonnamethres.png test.pokemonnamethres --tessdata-dir ./ -l jpn && cat test.pokemonnamethres.txt
 echo jpn.lato.exp0
 tesseract jpn.lato.exp0.tif test.lato --tessdata-dir ./ -l jpn && cat test.lato.txt
 echo jpn.latob.exp0
 tesseract jpn.latob.exp0.tif test.latob --tessdata-dir ./ -l jpn && cat test.latob.txt
 echo jpn.lato.kana
 tesseract jpn.lato.kana.exp0.tif test.lato.kana --tessdata-dir ./ -l jpn && cat test.lato.kana.txt
+echo jpn.latob.kana
+tesseract jpn.latob.kana.exp0.tif test.latob.kana --tessdata-dir ./ -l jpn && cat test.latob.kana.txt
 echo jpn.hiraginomarugothicpro
 tesseract jpn.hiraginomarugothicpro.exp0.tif test.hiraginomarugothicpro  --tessdata-dir ./ -l jpn && cat test.hiraginomarugothicpro.txt
 #tesseract test.tif test --tessdata-dir ./ -l jpn && cat test.txt
